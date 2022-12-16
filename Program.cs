941638a9c6211b56aaaf3b65abc39df77e544879
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 19:01:26 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Case #1: 2
+Case #2: 5
+Case #3: 7
+Case #4: 17
+Case #5: 7
+using System;
+using System.Text;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                stringBuilder.Append($"Case #{i + 1}: {int.Parse(s[0]) + int.Parse(s[1])}" + "\n");
+            }
+            Console.WriteLine(stringBuilder);
+        }
+    }
